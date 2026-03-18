8820ecedeb72f414cf30f1f0d227574e21e20fd4
Vaibhav82736 <bishtvaibhav448@gmail.com>
2026-03-19 01:32:39 +0530

    changes in app.py last line duplicate app.run removed

--- backend/app.py
+++ backend/app.py
@@ -94,10 +94,8 @@ def get_history():
     data = list(history.find({"user": user}, {"_id": 0}))
     return jsonify(data)
 
+import os
 
 if __name__ == "__main__":
-    app.run(port=5000)
-
-import os
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
