99004ca77481e0d55ea46b3fd9d235e77c391bcc
Vaibhav82736 <bishtvaibhav448@gmail.com>
2026-04-24 18:06:08 +0530

    commit

--- backend/app.py
+++ backend/app.py
@@ -186,5 +186,5 @@ def get_history():
 # ===================== RUN =====================
 if __name__ == "__main__":
     print("🚀 Starting Flask app...")
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5001))
     app.run(host="0.0.0.0", port=port)
